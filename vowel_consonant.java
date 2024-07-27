f02aa26642a586f609aa759c28b47c9892478a29
amish12822 <amishofficial23@gmail.com>
2024-07-27 13:36:00 +0530

    complete program to check vowel or consonenet letter.

---
+++ vowel_consonant.java
@@ -0,0 +1,17 @@
+/**
+ * vowel_consonant
+ */
+public class vowel_consonant {
+
+    public static void main(String[] args) {
+        char a = 'I';
+
+        if ( a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U' ){
+            System.out.println(a + " is a Vowel Letter.");
+        } else {
+            System.out.println(a + " is a Consonant Letter.");
+        }
+
+
+    }
+}
